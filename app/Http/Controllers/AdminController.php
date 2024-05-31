0add02a2bf4d930b08b694828c20b3210389f2a7
カプール リティカ <kapoor-ri@ibjapan.jp>
2024-05-31 17:23:54 +0900

    Initial Commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } //End Method
+
+    public function Profile()
+    {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+    } //End Method
+}
